a8fe5e82bcf7a638eb42a152809dff255859436b
gulpanjul <gulpa.andhikac@gmail.com>
2025-07-23 23:43:53 +0700

    feat: sp2-08-api-deployment-sprint-2

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -1,48 +1,70 @@
-import swaggerAutogen from "swagger-autogen";
+import swaggerAutogen from 'swagger-autogen';
 
 const doc = {
   info: {
-    version: "v0.0.1",
-    title: "Dokumentasi API ACARA",
-    description: "Dokumentasi API ACARA",
+    version: 'v0.0.1',
+    title: 'Dokumentasi API ACARA',
+    description: 'Dokumentasi API ACARA',
   },
   servers: [
     {
-      url: "http://localhost:3000/api",
-      description: "Local Server",
+      url: 'http://localhost:3000/api',
+      description: 'Local Server',
     },
     {
-      url: "https://be-acara-ebon.vercel.app/api",
-      description: "Deploy Server",
+      url: 'https://gulpanjul-be-acara.vercel.app/api',
+      description: 'Deploy Server',
     },
   ],
   components: {
     securitySchemes: {
       bearerAuth: {
-        type: "http",
-        scheme: "bearer",
+        type: 'http',
+        scheme: 'bearer',
       },
     },
     schemas: {
       LoginRequest: {
-        identifier: "adminacara",
-        password: "123456",
+        identifier: 'adminacara',
+        password: '123456',
       },
       RegisterRequest: {
-        fullName: "Admin",
-        username: "adminacara",
-        email: "test@mail.com",
-        password: "123456",
-        confirmPassword: "123456",
+        fullName: 'Admin',
+        username: 'adminacara',
+        email: 'test@mail.com',
+        password: '123456',
+        confirmPassword: '123456',
       },
       ActivationRequest: {
-        code: "abcdef",
+        code: 'abcdef',
+      },
+      CreateCategoryRequest: {
+        name: '',
+        description: '',
+        icon: '',
+      },
+      CreateEventRequest: {
+        name: '',
+        banner: 'fileUrl',
+        category: 'category ObjectID',
+        description: '',
+        startDate: 'yyyy-mm-dd hh:mm:ss',
+        endDate: 'yyyy-mm-dd hh:mm:ss',
+        location: {
+          region: 'region id',
+          coordinates: [0, 0],
+        },
+        isOnline: false,
+        isFeatured: false,
+      },
+      RemoveMediaRequest: {
+        fileUrl: '',
       },
     },
   },
 };
 
-const outputFile = "./swagger_output.json";
-const endpointsFiles = ["../routes/api.ts"];
+const outputFile = './swagger_output.json';
+const endpointsFiles = ['../routes/api.ts'];
 
-swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFiles, doc);
+swaggerAutogen({ openapi: '3.0.0' })(outputFile, endpointsFiles, doc);
